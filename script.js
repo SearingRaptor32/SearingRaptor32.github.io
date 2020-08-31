a984c6c5999261a13154e55265556e61a5dca17b
BuildTools <unconfigured@null.spigotmc.org>
2020-08-31 23:42:59 +0300

    Test

---
+++ script.js
@@ -0,0 +1,6 @@
+function redirect_a() {
+  window.location = "https://searingraptor32.github.io/index.html";
+}
+function redirect_b() {
+  window.location = "https://searingraptor32.github.io/index.about";
+}
